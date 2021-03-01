40568095b5860f26eedff2eaa5ac92e27e8945e0
Brian <b3d4o5@Ggmail.com>
2021-02-28 18:45:06 -0800

    loading

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import styled from 'styled-components'
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import LinearProgress from '@material-ui/core/LinearProgress';
 
 function App() {
   const [text, setText] = useState('')
@@ -26,12 +27,14 @@ function App() {
       <Header>
         <TextField label="Search for memes" variant="outlined" style={{width:'calc(100% - 110px)'}} 
           value={text} onChange={e=> setText(e.target.value)} autoFocus
-        />
+          onKeyPress={e=> e.key==='Enter' && getMemes()}
+       />
         <Button variant="contained" color="primary" style={{height:55, marginLeft:10, width:100}}
            disabled={!text || loading} onClick={getMemes}>
            Search
         </Button>
       </Header>
+      {loading && <LinearProgress />}
       <Body>
         {memes.map(m=> <Meme key={m.id} src={m.images.fixed_width.url} />)}
       </Body>
@@ -40,7 +43,11 @@ function App() {
 }
 
 const Meme = styled.img`
-  max-height:200px
+  max-height: 200px;
+  max-width: 200px;
+  min-width: 200px;
+  object-fit: cover;
+  margin: 5px;
 `
 const Wrap = styled.div`
   display:flex;
